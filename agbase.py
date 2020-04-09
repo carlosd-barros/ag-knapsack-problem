f4454183ced495448a92ab0a51ca2265e8f45025
carlosd-barros <barros.carlosds@gmail.com>
2020-04-09 12:04:37 -0300

    startando população e aplicando sortei brabor da roleta russa

--- agbase.py
+++ agbase.py
@@ -1,66 +1,39 @@
+import random as rd
+
 from typing import Tuple, List
-from random import randint, random, choice, uniform
-from string import ascii_letters, ascii_lowercase, ascii_uppercase
-
-from parse import binarioParaReal
 
 
-def numeroAleatorio(integer:int=2, decimal:int=2, r:bool=False) -> float:
-    """Recebe dois valores inteiros e retorna um valor real
-    onde o parametro 'inteiro' determina a quantidade de casas
-    a esquerda do ponto, 'decimal' determina a quantidade de
-    casas a direita do ponto e 'r' determina se o arredondamento
-    será ou não aplicado."""
-    num = random() * (10 ** integer)
+# Itens que podem ser add na mochila
+# [benefício, peso]
+ITENS = (
+    [5,7], [8,8], [3,4], [2,10],
+    [7,4], [9,6], [4,4], [5,7],
+)
+CAPACIDADE = 22 #capacidade da mochila
 
-    return round(num, decimal) if r else num
-
-
-def stringAleatoria(length:int=10) -> str:
-    """Recebe um valor inteiro e retorna
-    string com o tamanho do valor passado."""
-    return "".join( choice(ascii_letters) for x in range(length) )
+sortBeneficio = lambda x: x[1]
+sortPeso = lambda x: x[2]
+sortAvaliacao = lambda x: x[3]
+sortValorDeUtilidade = lambda x: x[1]/x[2]
 
 
 def gerarBinarioString(length:int=8) -> str:
     """Recebe um valor inteiro que vai determinar
     o tamanho do binário string que sera retornado."""
-    return "".join([ choice(['0','1']) for i in range(length) ])
-
-
-def gerarListaDeStrings(listLength=100, stringLength=10):
-    """Recebe dois valores interios e retorna uma lista de strings
-    onde 'listLength' vai determinar a quantidade de elementos da lista
-    e 'stringLength' o tamanho da string de cada elemento."""
-    return [ stringAleatoria(stringLength) for i in range(listLength) ]
-
-
-def stringParaLista(string:str, maxNum:int=100):
-    return [ string, uniform(0, maxNum) ]
-
-
-def matrizDeStrings(lista: List):
-    return [ stringParaLista(string) for string in lista ]
-
-
-def moduloAoQuadrado(value):
-    return abs(2 - value ** 2)
-
-
-def selecionaMelhores(populacao:List, quant:int=10) -> Tuple:
-    populacao.sort(key=lambda x: x[1], reverse=True)
-
-    return populacao[:quant]
+    return "".join([ rd.choice("01") for i in range(length) ])
 
 
 def mutacao(value:str) -> str:
-    index = randint(0, len(value)-1)
+    """Retorna a estring recebida com um de seus
+    valores alterados de forma aleatória."""
+    index = rd.randint(0, len(value)-1)
     value = list(value)
-    value[index] = '0' if value[index] == '1' else '1'
+    value[index] = "0" if value[index] == "1" else "1"
 
     return "".join(value)
 
-def crossover1(value1:str, value2:str) -> Tuple[str,str]:
+
+def crossover1(value1:str, value2:str) -> List:
     """Recebe dois binários string de 8 bits e retorna uma tupla com
     duas novamos strings sendo a primeira composta pelos 4 primeiros
     bits do value2 e 4 ultimos bits do value1 e a segunda composta
@@ -68,10 +41,10 @@ def crossover1(value1:str, value2:str) -> Tuple[str,str]:
     new_value1 = value2[:4] + value1[4:]
     new_value2 = value1[:4] + value2[4:]
 
-    return new_value1, new_value2
+    return [new_value1, new_value2]
 
 
-def crossover2(value1:str, value2:str) -> Tuple[str,str]:
+def crossover2(value1:str, value2:str) -> List:
     """Recebe dois binários string de 8 bits e retorna uma tupla com duas novamos
     strings sendo a primeira composta pelos 2 primeiros bits do value2 + os 4 bits do
     meio do value1 + os 2 ultimos bits do value2 e a segunda composta pelos 2 primeiros
@@ -79,45 +52,71 @@ def crossover2(value1:str, value2:str) -> Tuple[str,str]:
     new_value1 = value2[:2] + value1[2:6] + value2[6:]
     new_value2 = value1[:2] + value2[2:6] + value1[6:]
 
-    return new_value1, new_value2
+    return [new_value1, new_value2]
 
 
-def avaliacao(binary:str):
-    pass
+def calculaBeneficioEPeso(binario:str) -> List:
+    """Retorna um indivíduo com a seguinte estrutura
+    [binario:str, benefício:int, peso:int]."""
+    beneficio = 0
+    peso = 0
+
+    for index, bit in enumerate(binario):
+        if bit == '0': continue
+        b, p = ITENS[index]
+        beneficio += b
+        peso += p
+
+    return [binario, beneficio, peso]
 
 
-def roletaSelecao(populacao:List, quant:int):
-    sum_fitness = sum([ x[1] for x in populacao ])
-    num = choice([0,1])
+# Aptidão, Avaliação, fitness ou Objetivo
+def avaliacaoLinear(n:int, pos:int, sp:float) -> float:
+    """n: tamanho da população;
+    pos: posição do indivíduo;
+    sp: um valor qualquer entre 1 e 2."""
+    return abs( round(2 - sp + ( 2 * (sp - 1) * (pos - 1) ) / (n - 1), 4) )
 
-    return None
 
+def rankingSelecaoLinear(populacao:List, quant:int, ass:bool=True, tend:int=0.0001):
+    """população -> indivíduo: [binário:str, benefício:int, peso:int, avaliacao:float];
+    quant: quantidade de sorteios;
+    ass: define se a assimetria será positiva (True) ou negativa (False);
+    tend: define a variação da assimetria, onde 0 significa que o grau de desvio será nenhum."""
+    populacao.sort(key=sortAvaliacao)
+    Fi = populacao[0][3] - 0.001 # min
+    Fj = populacao[-1][3] + 0.001 # max
 
-def selecionaElementosAleatoriosEModifica(populacao:List, quant:int=10):
     results = []
-    length = len(populacao) - 1
-    index = randint(0, length)
+    for i in range(quant):
+        tendencia = (Fi + Fj) / 2
+        tendencia = tendencia + tend if ass else tendencia - tend
+        criterio = round( rd.triangular(low=Fi, high=Fj, mode=tendencia),4 )
+        # print(f"{i} criterio: {criterio}, ass: {ass}, tend: {tend}")
 
-    for i in range(max):
-        individuo = mutacao(populacao.pop(index)[0])
-        results.append(individuo)
-        length -= 1
-        index = randint(0, length)
+        for individuo in populacao:
+            binario, b, p, a = individuo
+            if a > criterio and p <= CAPACIDADE:
+                # print(f"sorteado: {individuo}\n","-"*30)
+                results.append(individuo)
+                break
 
+    results.sort(key=sortPeso)
     return results
 
 
-def criarPopulacaoAPartirDeIndividuosExistentes(individuos:List) -> List:
-    result = []
+def criarNovaGeracao(populacao:List, prob:int=1) -> List:
+    """Aplica a função de 'mutacao' em uma pocentagem (definida em 'prob')
+    aleatória dos novos indivíduos sendo gerados pelo crossover da 'populacao'.
+    Retorna uma lista com N * (N-1) indivíduos, onde N é o tamanho da 'populacao'."""
+    if prob >= 1: prob = prob/100
+    results = []
 
-    for index, value in enumerate(individuos):
-        for val in individuos[index+1:]:
-            result += crossover2(value[0], val[0])
+    for index, parent1 in enumerate(populacao):
+        for parent2 in populacao[index+1:]:
+            index = rd.choice([0,1])
+            childs = crossover1(parent1[0], parent2[0])
+            if rd.random() <= prob: childs[index] = mutacao(childs[index]);
+            results += childs
 
-    return result
-
-
-
-
-
-
+    return results
--- main.py
+++ main.py
@@ -1,60 +1,83 @@
-from agbase import (
-    criarPopulacaoAPartirDeIndividuosExistentes,
-    gerarBinarioString,
-)
+from random import uniform
+
+from agbase import CAPACIDADE as CAP
+from agbase import sortBeneficio, sortPeso, sortAvaliacao, sortValorDeUtilidade
+from agbase import gerarBinarioString, calculaBeneficioEPeso, rankingSelecaoLinear
+
 
 #####################################
 # ESTRUTURA DO ALGORITIMO GENÉTICO
-# 1- Inicialização da população
+# 1 - Inicialização da população | FEITO
 # 2 - Avaliação de cada indivíduo
-# 3 - Seleção de alguns indivíduos
+# 3 - Seleção de alguns indivíduos | FEITO
 # 4 - crossover e mutação
 # 5 - concepção da nova geração
 # repete a partir do passo 2 até estar satisfeito
 # 6 - fim do algoritimo
 #####################################
 
-sortBeneficio = lambda x: x[1]
-sortPeso = lambda x: x[2]
-
 # estrutura dos cromossomos:
-# [binário: str, benefício/fitness: int, peso: int]
+# [binário:str, benefício:int, peso:int, avaliacao:float]
 
 
-# população inicial
-# gerar uma população de forma randomica
-populacao_length = 100
-populacao = [ gerarBinarioString(length=8) for i in range(populacao_length) ]
+# 1 - Inicialização da população
+POPULACAO_LENGTH = 100
+BS = gerarBinarioString
+populacao = [ calculaBeneficioEPeso( BS(length=8) ) for i in range(POPULACAO_LENGTH) ]
+print("1 - Inicialização da população")
+print(f"populoção inicial criada >>> {populacao[-1]}, tamanho: {len(populacao)}","-"*50, sep="\n")
 
-# função de aptidão / fitness function
-# criar uma função que vai avaliar cada indivíduo
-# e recompensar os que estiverem melhor adaptados
-# e punir aqueles menos adaptados
-# algo semelhante a isso aqui
-# populacao = [ calcula_apttidao(individuo) for individuo in populacao ]
+qt_geracoes = 1000
+sp = round(uniform(1,2), 2)
+melhor_geracao = [0, 0, 0, 0, 0]# [bin, ben, peso, avaliacao, geracao]
 
+for i in range(2): # range(qt_geracoes):
+    if not isinstance(populacao[0], (list, tuple,)):
+        populacao = [ calculaBeneficioEPeso(x) for x in populacao ]
 
-# seleção
-# função para selecionar os indivíduos mais aptos da população
-# utilizando metodo da roleta para garantir a diversidade de indivíduos
-melhores = []
-
-# crossover
-# fazer o cruzamento dos indivíduos selecionados
-# no item anterior e gerar uma nova populaçao
-nova_populacao = criarPopulacaoAPartirDeIndividuosExistentes(melhores)
-
-# mutação
-# fazer uma função que recebe uma população e calcula a probabilidade
-# de um indivíduo da nossa população estar ou não apto a receber uma mutação
-# criar um calculo que determina se o indivíduo recebe ou não a mutação
-# taxa de mutação deve estar entre 0,001 e 0,1 dos indivíduos da população
+    # print("# 2 - Avaliação de cada indivíduo")
+    # os indivíduos devem ter essa estrutura: [binário:str, benefício:int, peso:int, avaliacao:float]
 
 
-melhor_global = [None, None, None]
-melhor_local = [None, None, None]
+    # print("#3 - Seleção de alguns indivíduos")
+    # precisa da dois pronta para funcionar
+    length, retries, quant, melhores = 0, 0, 10, []
 
-geracoes = int(input("Quantidade de gerações?\n"))
+    # while length != quant:
+    #     melhores = rankingSelecaoLinear(populacao=populacao, quant=quant)
+    #     length = len(melhores)
+    #     retries+=1
 
-# loop por tantas geerações repetindo tudo que foi feito
-# da função de aptidão em diante para cada geração
+
+    # pegando o melhor da geração
+    dif = melhor = None
+    for current_best in melhores:
+        if not melhor: melhor=current_best;dif=CAP-current_best[2];continue
+
+        binario, b, p, a = current_best
+        vu = b/p if b and p else 0
+        current_dif = CAP-p
+
+        if b > melhor[1] and p <= CAP:
+            melhor = current_best
+            dif = current_dif
+
+    # pegando o melhor de todas as gerações
+    if melhor and melhor[2] > melhor_geracao[2] and melhor[1] > melhor_geracao[1]:
+        melhor_geracao = melhor + [i]
+
+
+    # print("#4 - crossover e mutação")
+
+
+    # print("#5 - concepção da nova geração")
+    length, retries, nova_populacao = 0, 0, []
+
+
+    if melhor: print( f"Melhor da geração ({i}): - binário: {melhor[0]}, benefício: {melhor[1]}, peso: {melhor[2]}, avaliação: {round(melhor[3], 2)}." )
+
+
+# 6 - fim do algoritimo
+# printar o indivíduo ótimo e qual a sua geração
+print( "-"*50, f"\nMelhor indivíduo - Geração: {melhor_geracao[-1]}, binário: {melhor_geracao[0]}, benefício: {melhor_geracao[1]}, peso: {melhor_geracao[2]}, avaliação: {round(melhor_geracao[3], 2)}.")
+
